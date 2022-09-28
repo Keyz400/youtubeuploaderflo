1f9a8cd1f3669eba824e3ccdf455782b95bbc31f
Keyz400 <109141199+Keyz400@users.noreply.github.com>
2022-09-28 14:30:09 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,25 +2,25 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "5447871940:AAHqcFafAF9s9nopxdYbmWOZa7XliNs5vPg"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "anewytbyvineetbot"
 
-    API_ID = ""
+    API_ID = "7458995"
 
-    API_HASH = ""
+    API_HASH = "17e63b87ceb9047646f57b3a8bb07c45"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "544063816901-4fp5d5bgggj1at8p7br4hgmv1eq548mb.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [1318663278]
 
-    VIDEO_DESCRIPTION = ""
+    VIDEO_DESCRIPTION = "For more interesting videos subscribe our channel"
 
     VIDEO_CATEGORY = ""
 
-    VIDEO_TITLE_PREFIX = ""
+    VIDEO_TITLE_PREFIX = "The amazing shorts"
 
     VIDEO_TITLE_SUFFIX = ""
     
